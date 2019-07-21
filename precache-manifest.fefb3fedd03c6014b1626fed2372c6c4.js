f276b705ea86878a21b5530271311b4a19583079
Vinicius V. Parizoto <38093215+ParizotoV@users.noreply.github.com>
2019-07-21 01:59:19 -0300

    Updates

---
+++ precache-manifest.fefb3fedd03c6014b1626fed2372c6c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806ff505d0e459a44c56cf17d83ff88d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "243940405b7a5bd8f232",
+    "url": "/monsters-rolodex/static/css/main.a7fbba96.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "243940405b7a5bd8f232",
+    "url": "/monsters-rolodex/static/js/main.7bff5814.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
